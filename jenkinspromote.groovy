532f35c379557086fdc375266a12240f60647200
sai <sairohith77@gmail.com>
2022-10-19 12:09:26 +0000

    add5

--- jenkinspromote.groovy
+++ jenkinspromote.groovy
@@ -7,6 +7,7 @@ pipeline {
         stage ('vcs') {
             steps{
                 git url: 'https://github.com/rohi3369/shopizer.git'
+                     branch: 'promote'          
             }
         } 
          stage('merge'){
