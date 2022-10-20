2f55e33cd372569983d69dbcc78c9b3f3204fb34
sai <sairohith77@gmail.com>
2022-10-20 07:18:02 +0000

    jenkinspromote.groovy

---
+++ workspace/jenkinspromote.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    triggers {
+           cron('* 17 * * *')
+    }
+    stages {
+        stage ('vcs') {
+            steps{
+                git url: 'git@github.com:rohi3369/shopizer.git'
+            }
+        } 
+         stage('merge'){
+            steps{
+            sh 'git checkout release'
+            sh 'git merge develop --no-ff'
+            sh 'git push -u origin release'
+            }
+        }
+    }  
+    
+}
